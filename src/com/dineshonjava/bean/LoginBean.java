e92e419b8c2e03b024289c47edc849f2cb84f875
khedekarashwini <ashwinikhedekar24@gmail.com>
2019-11-22 13:40:03 +0530

    added project

---
+++ src/com/dineshonjava/bean/LoginBean.java
@@ -0,0 +1,21 @@
+package com.dineshonjava.bean;
+
+import org.apache.xpath.operations.String;
+
+public class LoginBean {
+  private Integer id=123;
+  private java.lang.String password="ashu";
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public java.lang.String getPassword() {
+	return password;
+}
+public void setPassword(java.lang.String password) {
+	this.password = password;
+}
+   
+}
